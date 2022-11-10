cae4fc9988c4f3d60ed784848546977c1cd19e85
Mamun2k22 <mamun180037@gmail.com>
2022-11-10 23:16:25 +0600

    Color change

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -14,17 +14,17 @@ const Blog = () => {
                     </div>
 
                     <div className='mb-10 bg-zinc-800 text-white p-8 rounded-lg '>
-                        <h2 className='text-2xl font-bold text-amber-300'>2.what is jwt and how does it work </h2>
+                        <h2 className='text-2xl font-bold'>2.what is jwt and how does it work </h2>
                         <p><span className='text-xl font-bold'>Ans:</span> <span className='text-xl font-semibold'></span>JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
                     </div>
 
                     <div className='mb-10 bg-zinc-800 text-white p-8 rounded-lg '>
-                        <h2 className='text-2xl font-bold text-amber-300'>3.what is the difference between javascript and node.js?</h2>
+                        <h2 className='text-2xl font-bold'>3.what is the difference between javascript and node.js?</h2>
                         <p><span className='text-xl font-bold'>Ans:</span> <span className='text-xl font-semibold'>The</span> JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.
                         </p>
                     </div>
                     <div className='mb-10 bg-zinc-800 text-white p-8 rounded-lg '>
-                        <h2 className='text-2xl font-bold text-amber-300'>4. How does NodeJS handle multiple requests at the same time?</h2>
+                        <h2 className='text-2xl font-bold'>4. How does NodeJS handle multiple requests at the same time?</h2>
                         <p><span className='text-xl font-bold'>Ans:</span> <span className='text-xl font-semibold'></span>NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.
                         </p>
                     </div>
